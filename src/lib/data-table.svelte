de1a4adac7051ea3aa321c30e99403993631d660
Eric Lifka <ericlifka@MacBook-Pro-de-Eric.local>
2023-04-04 12:46:31 -0400

    data table can sort on columns

--- src/lib/data-table.svelte
+++ src/lib/data-table.svelte
@@ -1,16 +1,36 @@
 <script>
     export let rows;
     export let headers;
+
+    let sortColumn = -1;
+    let sortReverse = false;
+
+    function changeSort(column) {
+        if (sortColumn === column) {
+            sortReverse = !sortReverse;
+        } else {
+            sortColumn = column;
+            sortReverse = false;
+        }
+    }
+
+    $: sorter = typeof rows[0][sortColumn] == "string"
+        ? (a, b) => a.localeCompare(b)
+        : (a, b) => a - b;
+
+    $: sortedRows = [...rows].sort((a, b) => sortReverse ? sorter(b[sortColumn], a[sortColumn]) : sorter(a[sortColumn], b[sortColumn]));
 </script>
 
 <table>
     <tr>
-        {#each headers as header}
-            <th>{header}</th>
+        {#each headers as header, i}
+            <th>
+                <button on:click={() => changeSort(i)}>{header}</button>
+            </th>
         {/each}
     </tr>
     
-    {#each rows as row}
+    {#each sortedRows as row}
         <tr>
             {#each row as cell}
                 <td>{cell}</td>
